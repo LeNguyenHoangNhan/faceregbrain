da78f92f445e2ef0232a17ad40b86c1b836392bf
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2020-03-20 22:43:23 +0700

    Updates

---
+++ precache-manifest.09b185fb1ebe2d8b9637ae14e5fab74d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6a4efc0ca824916a99a49a61766b97",
+    "url": "/faceregbrain/index.html"
+  },
+  {
+    "revision": "97fca8ee2ad081a53d06",
+    "url": "/faceregbrain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "23e8aedfeebdb0949476",
+    "url": "/faceregbrain/static/css/main.a4461a71.chunk.css"
+  },
+  {
+    "revision": "97fca8ee2ad081a53d06",
+    "url": "/faceregbrain/static/js/2.73d5ba4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faceregbrain/static/js/2.73d5ba4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e8aedfeebdb0949476",
+    "url": "/faceregbrain/static/js/main.817fcc44.chunk.js"
+  },
+  {
+    "revision": "8d491e9718d40213c74b",
+    "url": "/faceregbrain/static/js/runtime-main.ee534faa.js"
+  },
+  {
+    "revision": "160724aa0b0d79eaf19ea29b110b016a",
+    "url": "/faceregbrain/static/media/brain.160724aa.png"
+  }
+]);
